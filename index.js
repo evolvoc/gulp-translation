4c2713f062e52d7bf1a4e75dd9f1fb036e308c7c
Mastilver <th.sileghem@gmail.com>
2014-03-15 17:23:21 +0100

    Fix: It now output all the requested file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var through = require('through2');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 var File = require('vinyl');
+var async = require('async');
 
 const PLUGIN_NAME = 'gulp-translation';
 
@@ -33,16 +34,8 @@ var translate = function(opt)
 
 		var self = this;
 
-		opt.localeFiles.pipe(gutil.buffer(function(err, localeFiles)
+		getLocales(opt.localeFiles, function(err, locales)
 		{
-			// get locales configuration
-			var locales = {};
-			localeFiles.forEach(function(localeFile)
-			{
-				locales[localeFile.relative.split('.')[0]] = JSON.parse(localeFile.contents.toString('utf8'));
-			});
-
-
 			for(var locale in locales)
 			{
 				var text = file.contents.toString('utf8');
@@ -97,12 +90,27 @@ var translate = function(opt)
 			}
 
 			callback();
-		}));
+		});
 	};
 
 
 	return through.obj(transform);
 };
+
+
+var getLocales = async.memoize(function(localeFiles, callback)
+{
+	localeFiles.pipe(gutil.buffer(function(err, localeFiles)
+	{
+		var locales = {};
+		localeFiles.forEach(function(localeFile)
+		{
+			locales[localeFile.relative.split('.')[0]] = JSON.parse(localeFile.contents.toString('utf8'));
+		});
+
+		callback(null, locales);
+	}));
+}, function(){return 1});
 
 
 module.exports = translate;
